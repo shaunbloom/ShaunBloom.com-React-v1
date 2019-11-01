3a270bf4aa54f7369d37d88840c6fea2ffa45156
Shaun Bloom <shaunbloom@shauns-air.lan>
2019-11-01 13:39:20 -0700

    fixed nav css and added build folder back

--- source/components/Application.js
+++ source/components/Application.js
@@ -100,7 +100,7 @@ var Application = React.createClass({
 					<ul>
 			            <li><a href="http://www.shaunbloom.com" title="Original site built with JQuery in '12">Jquery '12</a></li>
 			            <li><a href="http://angular.shaunbloom.com" title="Rebuilt site in Angular to look and function exactly the same as the original JQuery version">Angular '15</a></li>
-			            <li><a href="http://react.shaunbloom.com" class="selected" title="Original React (V 0.14) build out from 2015. Made to look and function exactly as the original JQuery version.">React '15</a></li>
+			            <li><a href="http://react.shaunbloom.com" className="selected" title="Original React (V 0.14) build out from 2015. Made to look and function exactly as the original JQuery version.">React '15</a></li>
 			            <li><a href="http://reactv2.shaunbloom.com" title="Latest React build (V 16) using webpack, babel and es6. Made to look and function exactly as the original JQuery version.">React v2 '19</a></li>
 			            <li><a href="http://vanillajs.shaunbloom.com" title="Latest build with Webpack, Babel and less using the latest vanilla ES6. Made to look and function exactly as the original JQuery version.">Vanilla '19</a></li>
 			        </ul>
